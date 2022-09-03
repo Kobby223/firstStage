1eac37a2dcdc83d91100fe4c919be9ce0822f927
Kobby223 <iantubers957@gmail.com>
2022-09-03 01:59:08 +0000

    First Stage

---
+++ concept/assets/libs/js/main-js.js
@@ -0,0 +1,11 @@
+
+jQuery(document).ready(function($) {
+    'use strict';
+    // Menu Slim Scroll List
+    if ($(".menu-list").length) {
+        $('.menu-list').slimScroll({
+
+        });
+    }
+
+}); // AND OF JQUERY
